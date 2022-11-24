855a160a28fb8579c9d16c2c1f2665e22e861fe7
SaltyNaca <115601749+SaltyNaca@users.noreply.github.com>
2022-11-24 17:35:53 -0300

    Added Php Files

---
+++ includes/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbservername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "subohmc_db_store";
+
+    $connect=mysqli_connect($dbservername, $dbUsername, $dbPassword, $dbName);
+?>
