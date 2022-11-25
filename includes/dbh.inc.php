59ed16fdfa7ecec6fc540242d0a7a6f2674d8403
SaltyNaca <115601749+SaltyNaca@users.noreply.github.com>
2022-11-25 19:14:10 -0300

    Addeds Sales y Esencias Page

--- includes/dbh.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $dbservername = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbName = "subohmc_db_store";
-
-    $connect=mysqli_connect($dbservername, $dbUsername, $dbPassword, $dbName);
-?>
